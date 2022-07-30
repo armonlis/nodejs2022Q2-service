b34b5a433db4f407276dc91df638152b44824e84
Siarhei Khonski <armonius@mail.ru>
2022-07-30 16:16:09 +0300

    feat: implement logging into files

--- src/logging/log.service.ts
+++ src/logging/log.service.ts
@@ -1,5 +1,9 @@
 import { LoggerService, Injectable } from '@nestjs/common';
 import { stdout } from 'process';
+import { FileWriter } from './writer';
+
+const writer = new FileWriter();
+(async () => await writer.initializing())();
 
 const log_levels = ['log', 'error', 'warn', 'debug', 'verbose'];
 const LOG_LEVEL =
@@ -16,21 +20,27 @@ export class LogService implements LoggerService {
   private readonly level: string[];
   private readonly mode: 'console' | 'file';
   private readonly target: any;
+  private readonly writer: FileWriter;
 
   constructor() {
     this.level = LOG_LEVEL;
     this.mode = LOG_MODE;
+    if (this.mode === "file") {
+      this.writer = writer;
+    }
   }
 
   log(message: string) {
+    const data = ` [LOG]  ${new Date().toLocaleString()} - ${message}\n`
     switch (this.mode) {
       case 'console':
         {
-          stdout.write(` [LOG]  ${new Date().toLocaleString()} - ${message}\n`);
+          stdout.write(data);
         }
         break;
       case 'file':
         {
+          this.writer.write(data);
         }
         break;
       default:
@@ -42,14 +52,17 @@ export class LogService implements LoggerService {
     if (this.level.length < 2) {
       return;
     }
+    const data = `[ERROR] ${new Date().toLocaleString()} - ${message}\n`
     switch (this.mode) {
       case 'console':
         {
-          stdout.write(`[ERROR] ${new Date().toLocaleString()} - ${message}\n`);
+          stdout.write(data);
         }
         break;
       case 'file':
         {
+          this.writer.write(data);
+          this.writer.writeError(data);
         }
         break;
       default:
@@ -61,14 +74,16 @@ export class LogService implements LoggerService {
     if (this.level.length < 3) {
       return;
     }
+    const data = ` [WARN] ${new Date().toLocaleString()} - ${message}\n`
     switch (this.mode) {
       case 'console':
         {
-          stdout.write(` [WARN] ${new Date().toLocaleString()} - ${message}\n`);
+          stdout.write(data);
         }
         break;
       case 'file':
         {
+          this.writer.write(data);
         }
         break;
       default:
@@ -77,17 +92,19 @@ export class LogService implements LoggerService {
   }
 
   debug(message: string) {
-    if (this.level.length < 3) {
+    if (this.level.length < 4) {
       return;
     }
+    const data = ` [DEBUG] ${new Date().toLocaleString()} - ${message}\n`
     switch (this.mode) {
       case 'console':
         {
-          stdout.write(` [WARN] ${new Date().toLocaleString()} - ${message}\n`);
+          stdout.write(data);
         }
         break;
       case 'file':
         {
+          this.writer.write(data);
         }
         break;
       default:
@@ -96,17 +113,19 @@ export class LogService implements LoggerService {
   }
 
   verbose(message: string) {
-    if (this.level.length < 3) {
+    if (this.level.length < 5) {
       return;
     }
+    const data = `[VERBOSE] ${new Date().toLocaleString()} - ${message}\n`
     switch (this.mode) {
       case 'console':
         {
-          stdout.write(` [WARN] ${new Date().toLocaleString()} - ${message}\n`);
+          stdout.write(data);
         }
         break;
       case 'file':
         {
+          this.writer.write(data);
         }
         break;
       default:
